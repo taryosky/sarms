f285427a0c0dca7b180a86e9093ee99b5d84a15f
wokoro douye samuel <wokorosamuel@yahoo.com>
2018-01-15 02:45:15 -0800

    time table update, hall constrain added(no two course would be fixed on thesame period)

--- app/controllers/admin_activities_controller.rb
+++ app/controllers/admin_activities_controller.rb
@@ -227,7 +227,7 @@ class AdminActivitiesController < ApplicationController
           status = true
           if ttable.size > 0
             ttable.each do |time_table|
-              if time_table.period.to_s == per && Course.find_by(id: time_table.course_id).level == Course.find_by(id: id).level
+              if time_table.period.to_s == per && (Course.find_by(id: time_table.course_id).level == Course.find_by(id: id).level || time_table.hall==hall)
                   status = false
               end
             end
@@ -283,7 +283,7 @@ class AdminActivitiesController < ApplicationController
               status = true
             if ttable.size > 0
               ttable.each do |time_table|
-                if time_table.period.to_s == per && Course.find_by(id: time_table.course_id).level == Course.find_by(id: id).level
+                if time_table.period.to_s == per && (Course.find_by(id: time_table.course_id).level == Course.find_by(id: id).level || time_table.hall==hall)
                     status = false
                 end
               end
